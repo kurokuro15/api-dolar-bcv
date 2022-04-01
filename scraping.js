bc4fb9b66b429b43dcef50e1240f68f2cafbaea8
Reynaldo González <kurokuro15000@gmail.com>
2022-04-01 15:01:42 -0400

    create scraping function

---
+++ scraping.js
@@ -0,0 +1,59 @@
+const puppeteer = require('puppeteer')
+const jsdom = require('jsdom')
+const { query } = require('./conection.js')
+
+
+const scraping = async () => {
+	const insert = async obj => {
+		const timestamp = new Date()
+		const { usd, eur } = obj
+		const queryString = {
+			dolar: `INSERT INTO dolar(value,time) VALUES(?,?)`,
+			euro: `INSERT INTO euro(value,time) VALUES(?,?)`
+		}
+		const q1 = await query(queryString['dolar'], [usd, timestamp])
+		const q2 = await query(queryString['euro'], [eur, timestamp])
+		console.log({q1,q2})
+	}
+	
+  try {
+    // Abrimos una instancia del puppeteer y accedemos a la url de google
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    const response = await page.goto('http://www.bcv.org.ve/')
+    const body = await response.text()
+
+    // Creamos una instancia del resultado devuelto por puppeter para parsearlo con jsdom
+    const {
+      window: { document }
+    } = new jsdom.JSDOM(body)
+
+    // Seleccionamos los titulos y lo mostramos en consola
+    const currencies = [
+      document.querySelector('div#dolar strong').textContent,
+      document.querySelector('div#euro strong').textContent
+    ]
+    const res = currencies.map(string => {
+      return Number(string.replace(',', '.'))
+    })
+    const obj = { usd: res[0], eur: res[1] }
+    await insert(obj)
+    // Cerramos el puppeteer
+    await browser.close()
+		return 'Página web escarbada y datos almacenados en la db.'
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function get (table) {
+	const queryString = `SELECT value as ${table} FROM ${table} WHERE time = MAX(time)`
+	result = await query(queryString)
+	return result
+}
+
+async function execute (){
+	const result = await scraping()
+	console.info(result)
+}
+module.exports = {get, execute }
